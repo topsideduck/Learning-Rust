6dbd0f801b9b95816cf055885b0bfc6e00e4af4c
Harikeshav R <r.harikeshav@gmail.com>
2022-11-29 21:33:26 +0530

    Data Types

---
+++ 3-Common-Programming-Concepts/data_types/src/main.rs
@@ -0,0 +1,55 @@
+fn main() {
+    let x: u32 = 5;
+
+    let y: f64 = 10.56;
+
+    let sum = 5 + 10;
+
+    let difference = 95.5 - 4.3;
+
+    let product = 4 * 30;
+
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+
+    let remainder = 43 % 5;
+
+    let t = true;
+
+    let f: bool = false;
+
+    let small_z = 'z';
+
+    let capital_z: char = 'ℤ'; // with explicit type annotation
+
+    let tuple: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = tuple.0;
+
+    let six_point_four = tuple.1;
+
+    let one = tuple.2;
+
+    let array = [1, 2, 3, 4, 5];
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let one_to_five: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let five_threes = [3; 5];
+
+    
+}
