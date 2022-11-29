6b174bd4439d746f4b1ee69f82e785b5dbeadccd
Harikeshav R <r.harikeshav@gmail.com>
2022-11-29 22:08:29 +0530

    Functions

---
+++ 3-Common-Programming-Concepts/functions/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
